40f4fbc244aba9f69ec0e47cabebf5fa87489522
andreddd799 <36064944+andreddd799@users.noreply.github.com>
2020-11-17 16:06:11 +0300

    Домашнее задание к уроку 3 (Первое занятие с Алексеем Виноградовым)

--- src/test/java/tests/AlfaBankTests.java
+++ src/test/java/tests/AlfaBankTests.java
@@ -1,24 +1,60 @@
 package tests;
 
-import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class AlfaBankTests {
 
-    @Test
-    void openVkladiTab() {
-        open("https://alfabank.ru");
-        $$(byText("Вклады")).find(visible).click();
-        $("#breadcrumbs").$(byText("Счета и депозиты")).shouldBe(visible);
-        $(byText("Архивные счета и депозиты")).click();
-        $$(byText("Депозиты")).find(visible).click();
-        int a = $("#filter").findAll(".m23QzrO").size();
-        System.out.println("Количество элементов: " + a);
-//        $("#filter").find(".m23QzrO").getValue();
+    String testURL = "https://alfabank.ru";
+
+    @BeforeAll
+    static void setUp() {
+        Configuration.startMaximized = true;
     }
 
+    @Test
+    void checkNumberOfArchivedDeposits() {
+        int expectedNumber = 5;
+
+        open(testURL);
+        $$(byText("Вклады")).find(visible).click();
+        $("#breadcrumbs").$(byText("Счета и депозиты")).shouldBe(visible);
+        $$(byText("Депозиты")).find(visible).parent().click();
+        $(byText("Архивные счета и депозиты")).click();
+        $$(byText("Депозиты")).find(visible).click();
+        $("#filter").findAll("[data-widget-name=CatalogCard]").shouldHaveSize(expectedNumber);
+    }
+
+    @Test
+    void vkladyiStrahovanieSiblingPreceding() {
+        open(testURL);
+        $$(byText("Вклады")).find(visible).click();
+        SelenideElement element = $("[data-test-id=tabs-list-tabTitle-0]").sibling(1).preceding(0);
+        element.click();
+        element.shouldHave(cssValue("background-color", "rgba(11, 31, 53, 1)"));
+    }
+
+    @Test
+    void vkladyiStrahovanieClosest() {
+        open(testURL);
+        $$(byText("Вклады")).find(visible).click();
+        SelenideElement element = $("[data-test-id=tabs-list-tabTitle-1]");
+        element.click();
+        element.closest("div").shouldHave(attribute("data-test-id", "tabs-list"));
+    }
+
+    @Test
+    void vkladyiStrahovanieParent() {
+        open(testURL);
+        $$(byText("Вклады")).find(visible).click();
+        SelenideElement element = $("[data-test-id=tabs-list-tabTitle-1]");
+        element.click();
+        element.parent().shouldHave(attribute("data-test-id", "tabs-list"));
+    }
 }
